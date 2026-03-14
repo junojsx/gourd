9da9d46030c098218878a2433e3682966a0c5292
junojsx <junojsx@gmail.com>
2026-03-13 22:08:08 -0700

    Initial Commit

---
+++ milk/milkApp.swift
@@ -0,0 +1,17 @@
+//
+//  milkApp.swift
+//  milk
+//
+//  Created by Justin Aquino on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct milkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
